45651d3c51c2ddc2cc7327d32289d22e4158252c
Urmas Rist <urmas@urist.ee>
2022-12-20 14:15:49 +0200

    add docker, proxy vite developer server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+            "/api": {
+                target: 'http://localhost:7070',
+                changeOrigin: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
+    },
     plugins: [vue()],
 });
